83dd2f0900d63228228d70a5451a46e613dff200
Alexander Whan <whan.alexander@gmail.com>
2020-09-26 20:46:59 -0700

    Raised max visible todo items per page from 3 -> 5

--- src/components/todo/context/settings.js
+++ src/components/todo/context/settings.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export const SettingsContext = React.createContext();
 
 function Settings(props) {
-  const [maxVisible, setMaxVisible] = useState(3);
+  const [maxVisible, setMaxVisible] = useState(5);
   const [showCompleted, setShowCompleted] = useState(true);
 
   // props.children will tell the context provider to render all child components regardless of what gets wrapped in it
